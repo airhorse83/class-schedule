c656cff530d6115903697d6c340549e56c80c618
airhorse83 <airhorse83@yahoo.com>
2018-12-04 16:01:54 -0700

    added Fontawesome and built custom icon comp

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-
-class LibraryCourse extends Component {
+ import Icon from '../icon';
+ class LibraryCourse extends Component {
     render() {
-        return(
-            <div className='library-course'>
-                <label className="library-course__title">Problem Solving</label>
-                
-
+        return (
+            <div className="library-course">
+                <label className="library-course__title">Problem Solving</label> 
+                { Icon('fas fa-check', 'library-course__icon') }
+                {/* arrow component */}
+                { Icon('fas fa-plus-circle', 'library-course__action')}
                 <div className="library-course__description">
-                    <label>Course Description</label>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id ultrices nisl. Nulla nec sem suscipit, aliquam turpis sed, egestas risus. Integer laoreet sapien arcu, dapibus consectetur ligula feugiat et.</p>
-                </div>
-            </div>
-        )
-    }
-}
-
-export default LibraryCourse;
+                     <label>Course Description</label>
+                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fringilla facilisis mi, non pellentesque metus consectetur vel. Quisque dictum, lectus vitae dignissim tincidunt, sapien nibh placerat diam, quis blandit enim nulla in felis.</p>
+                 </div>
+             </div>
+         )
+     }
+ }
+ 
+ export default LibraryCourse;
