002d6a337c54bd81e0b60695f5ee9e9d535bd940
airhorse83 <airhorse83@yahoo.com>
2018-12-07 20:00:54 -0700

    finished schedule and action

--- src/components/action.js
+++ src/components/action.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
-import Icon from './icon';
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
-class Action extends Component {
-    render() {
-        return (
-            <div className={`${this.props.className} action`}>
-                { Icon('fas fa-plus-circle') }
-                {/* fas fa-times-circl */}
-            </div>
-        )
-    }
+import Action from '../action';
+
+class ScheduleCourse extends Component {
+  render() {
+    return (
+      <div className="schedule-course">
+        <div className="schedule-course__title">{this.props.title}</div>
+        <Action onClick={() => this.props.toggleEnrolled(this.props.id)} className="schedule-course__action action-remove"/>
+      </div>
+    );
+  }
 }
 
-export default Action;
+export default connect(null, actions)(ScheduleCourse);
